3758a8c5022faf00197522d21f56b37ebe1385c0
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2025-11-04 10:23:55 -0500

    Semana 11

--- Semana11-04NOV25/educa/src/main/java/pe/edu/uni/educa/rest/ConsultasRest.java
+++ Semana11-04NOV25/educa/src/main/java/pe/edu/uni/educa/rest/ConsultasRest.java
@@ -2,9 +2,13 @@ package pe.edu.uni.educa.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.uni.educa.service.ConsultasService;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/educa")
@@ -33,5 +37,17 @@ public class ConsultasRest {
         }
     }
 
+    @GetMapping("/matricula/listado/{idCurso}")
+    public ResponseEntity<?> listaMatriculados(@PathVariable int idCurso) {
+        try {
+            List<Map<String, Object>> matriculados = consultasService.listaMatriculados(idCurso);
+            return ResponseEntity.ok(matriculados);
+
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("error", e.getMessage()));
+        }
+    }
 
 }
--- Semana11-04NOV25/educa/src/main/java/pe/edu/uni/educa/service/ConsultasService.java
+++ Semana11-04NOV25/educa/src/main/java/pe/edu/uni/educa/service/ConsultasService.java
@@ -5,6 +5,10 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 @Service
 public class ConsultasService {
 
@@ -17,7 +21,6 @@ public class ConsultasService {
         return precio;
     }
 
-
     public int matriculados(int idCurso){
         // Validar codigo
         if(!this.existeCurso(idCurso)){
@@ -29,10 +32,31 @@ public class ConsultasService {
         return matriculados;
     }
 
-
     private boolean existeCurso(int idCurso){
         String sql = "select COUNT(1) contador from CURSO where cur_id=?";
         Integer contador = jdbcTemplate.queryForObject(sql, Integer.class, idCurso);
         return (contador==1);
     }
+
+    public List<Map<String, Object>> listaMatriculados(int idCurso){
+        // Validar codigo
+        if(!this.existeCurso(idCurso)){
+            throw new RuntimeException("No existe el codigo del curso");
+        }
+        // Proceso
+        String sql = """
+                select
+                	m.cur_id idCurso, m.alu_id idAlumno,
+                	a.alu_nombre nombre, m.mat_tipo tipo,
+                	m.mat_precio precio, m.mat_cuotas cuotas,
+                	m.mat_nota nota
+                from MATRICULA m
+                join ALUMNO a on m.alu_id = a.alu_id
+                where m.cur_id = ?
+                """;
+        List<Map<String,Object>> lista = jdbcTemplate.queryForList(sql, idCurso );
+        return lista;
+    }
+
+
 }
